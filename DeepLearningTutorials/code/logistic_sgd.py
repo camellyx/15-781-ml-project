58e4a4eda68acc6518fbabf5436d48c1f1879dc4
Fan Yang <fanyang@Fan.local>
2014-11-02 22:53:48 -0500

    no comment

--- DeepLearningTutorials/code/logistic_sgd.py
+++ DeepLearningTutorials/code/logistic_sgd.py
@@ -326,10 +326,6 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
     # specify how to update the parameters of the model as a list of
     # (variable, update expression) pairs.
 
-    ### Modified updates function ###
-
-
-
     updates = [(classifier.W, classifier.W - learning_rate * g_W),
                (classifier.b, classifier.b - learning_rate * g_b)]
 
