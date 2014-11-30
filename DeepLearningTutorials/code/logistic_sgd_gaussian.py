30e441416d531ab9b10c1cd79ab30264ac1b24c3
Fan Yang <fanyang@Fan.local>
2014-11-29 20:21:37 -0500

    configured to run cifar-100

--- DeepLearningTutorials/code/logistic_sgd_gaussian.py
+++ DeepLearningTutorials/code/logistic_sgd_gaussian.py
@@ -46,6 +46,8 @@ import theano
 import theano.tensor as T
 from theano.tensor.shared_randomstreams import RandomStreams
 
+from logistic_sgd import LogisticRegression, load_data
+
 class LogisticRegression(object):
     """Multi-class Logistic Regression Class
 
@@ -165,86 +167,6 @@ class LogisticRegression(object):
         else:
             raise NotImplementedError()
 
-
-def load_data(dataset):
-    ''' Loads the dataset
-
-    :type dataset: string
-    :param dataset: the path to the dataset (here MNIST)
-    '''
-
-    #############
-    # LOAD DATA #
-    #############
-
-    # Download the MNIST dataset if it is not present
-    data_dir, data_file = os.path.split(dataset)
-    if data_dir == "" and not os.path.isfile(dataset):
-        # Check if dataset is in the data directory.
-        new_path = os.path.join(
-            os.path.split(__file__)[0],
-            "..",
-            "data",
-            dataset
-        )
-        if os.path.isfile(new_path) or data_file == 'mnist.pkl.gz':
-            dataset = new_path
-
-    if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
-        import urllib
-        origin = (
-            'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
-        )
-        print 'Downloading data from %s' % origin
-        urllib.urlretrieve(origin, dataset)
-
-    print '... loading data'
-
-    # Load the dataset
-    f = gzip.open(dataset, 'rb')
-    train_set, valid_set, test_set = cPickle.load(f)
-    f.close()
-    #train_set, valid_set, test_set format: tuple(input, target)
-    #input is an numpy.ndarray of 2 dimensions (a matrix)
-    #witch row's correspond to an example. target is a
-    #numpy.ndarray of 1 dimensions (vector)) that have the same length as
-    #the number of rows in the input. It should give the target
-    #target to the example with the same index in the input.
-
-    def shared_dataset(data_xy, borrow=True):
-        """ Function that loads the dataset into shared variables
-
-        The reason we store our dataset in shared variables is to allow
-        Theano to copy it into the GPU memory (when code is run on GPU).
-        Since copying data into the GPU is slow, copying a minibatch everytime
-        is needed (the default behaviour if the data is not in a shared
-        variable) would lead to a large decrease in performance.
-        """
-        data_x, data_y = data_xy
-        shared_x = theano.shared(numpy.asarray(data_x,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        shared_y = theano.shared(numpy.asarray(data_y,
-                                               dtype=theano.config.floatX),
-                                 borrow=borrow)
-        # When storing data on the GPU it has to be stored as floats
-        # therefore we will store the labels as ``floatX`` as well
-        # (``shared_y`` does exactly that). But during our computations
-        # we need them as ints (we use labels as index, and if they are
-        # floats it doesn't make sense) therefore instead of returning
-        # ``shared_y`` we will have to cast it to int. This little hack
-        # lets ous get around this issue
-        return shared_x, T.cast(shared_y, 'int32')
-
-    test_set_x, test_set_y = shared_dataset(test_set)
-    valid_set_x, valid_set_y = shared_dataset(valid_set)
-    train_set_x, train_set_y = shared_dataset(train_set)
-
-    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
-            (test_set_x, test_set_y)]
-    return rval
-
-
 def sgd_optimization_mnist(learning_rate=0.13, n_epochs=500,
                            dataset='mnist.pkl.gz',
                            batch_size=600):
@@ -292,7 +214,10 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=500,
 
     # construct the logistic regression class
     # Each MNIST image has size 28*28
-    classifier = LogisticRegression(input=x, n_in=28 * 28, n_out=10)
+    n_in = train_set_x.get_value(borrow=True).shape[1]
+    n_out = max(train_set_y.eval()) - min(train_set_y.eval()) + 1
+    # print n_in, n_out
+    classifier = LogisticRegression(input=x, n_in=n_in, n_out=n_out)
 
     # the cost we minimize during training is the negative log likelihood of
     # the model in symbolic format
@@ -322,10 +247,10 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=500,
     g_W = T.grad(cost=cost, wrt=classifier.W)
     g_b = T.grad(cost=cost, wrt=classifier.b)
 
-    # adding gaussian learning rate
-    srng = RandomStreams(seed=234)
-    learning_rate_W = srng.normal((28*28, 10), 2*learning_rate, learning_rate)
-    learning_rate_b = srng.normal((10,), 2*learning_rate, learning_rate)
+    # adding norml noise to learning rate of W and b
+    srng = RandomStreams(seed = 508)
+    learning_rate_W = srng.normal((n_in, n_out), 2*learning_rate, learning_rate)
+    learning_rate_b = srng.normal((n_out,), 2*learning_rate, learning_rate)
 
     # start-snippet-3
     # specify how to update the parameters of the model as a list of
@@ -408,14 +333,14 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=500,
                     test_score = numpy.mean(test_losses)
 
                     print(
-                       #(
-                       #    '     epoch %i, minibatch %i/%i, test error of'
-                       #     ' best model %f %%'
-                       # ) %
-                       # (
-                           # epoch,
-                           # minibatch_index + 1,
-                           # n_train_batches,'''
+                        #(
+                        #    '     epoch %i, minibatch %i/%i, test error of'
+                        #    ' best model %f %%'
+                        #) %
+                        #(
+                            #epoch,
+                            #minibatch_index + 1,
+                            #n_train_batches,
                             test_score * 100.
                         #)
                     )
@@ -425,7 +350,7 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=500,
                 break
 
     end_time = time.clock()
-    '''print(
+    ''' print(
         (
             'Optimization complete with best validation score of %f %%,'
             'with test performance %f %%'
@@ -439,4 +364,4 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=500,
                           ' ran for %.1fs' % ((end_time - start_time)))
 
 if __name__ == '__main__':
-    sgd_optimization_mnist()
+    sgd_optimization_mnist(dataset='cifar-10-python.tar.gz')
